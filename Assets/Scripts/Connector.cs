f4a54b888f6020d55f017064b88beb64c938064b
AmdHamdani <Hamdaniryuuken@gmail.com>
2020-06-14 15:38:38 +0700

    Add Connector

---
+++ Assets/Scripts/Connector.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Connector : MonoBehaviour
+{
+
+    public List<InputNode> InputNodes { get; private set; }
+    public List<OperatorNode> OperatorNodes { get; private set; }
+
+    private void Start() {
+        InputNodes = new List<InputNode>();
+        OperatorNodes = new List<OperatorNode>();
+    }
+
+    private void Update() {
+        InputNodes.ForEach((input) => Debug.Log(input.IsActive));
+    }
+
+    public void AddInput(InputNode inputNode) {
+        InputNodes.Add(inputNode);
+    }
+
+    public void AddOperator(OperatorNode operatorNode) {
+        OperatorNodes.Add(operatorNode);
+        Debug.Log("Add Nodes");
+    }
+}
