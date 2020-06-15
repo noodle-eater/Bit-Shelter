d01229eeadb9af7284f3154f0e4a62d5ff50692f
AmdHamdani <Hamdaniryuuken@gmail.com>
2020-06-15 23:00:41 +0700

    Add NodeData

---
+++ Assets/Scripts/NodeData.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class NodeData : MonoBehaviour
+{
+
+    public float YPosition;
+    public string NodeTag;
+    public NodeType Type;
+
+    private void Awake() {
+        YPosition = transform.position.y;
+        NodeTag = tag;
+        Type = GetComponent<INodeType>().GetNodeType();
+    }
+}
