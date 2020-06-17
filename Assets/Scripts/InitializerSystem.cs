92f95ee5cb643aa05239b688188da1c0fe2fc8cb
AmdHamdani <Hamdaniryuuken@gmail.com>
2020-06-17 22:53:08 +0700

    Add InitializerSystem

---
+++ Assets/Scripts/InitializerSystem.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using UnityEngine;
+
+public class InitializerSystem : MonoBehaviour
+{
+    private void Awake() {
+        
+    }
+
+    private void Start() {
+        FindObjectsOfType<InputNode>().ToList().ForEach((input) => input.InitOnStart());
+        FindObjectsOfType<OperatorNode>().ToList().ForEach((op) => op.InitOnStart());
+        FindObjectsOfType<StorageNode>().ToList().ForEach((storage) => storage.InitOnStart());
+        FindObjectsOfType<NodeData>().ToList().ForEach((node) => node.InitOnStart());
+        FindObjectOfType<NodeDataManager>().InitOnStart();
+        FindObjectsOfType<Connector>().ToList().ForEach((connector) => connector.InitOnStart());
+    }
+}
