e96fed4f737a6e7d51e4173856c3cf574a7e21e4
AmdHamdani <Hamdaniryuuken@gmail.com>
2020-06-17 21:59:35 +0700

    Add XPosition to NodeData

--- Assets/Scripts/NodeData.cs
+++ Assets/Scripts/NodeData.cs
@@ -4,11 +4,13 @@ public class NodeData : MonoBehaviour
 {
 
     public float YPosition;
+    public float XPosition;
     public string NodeTag;
     public NodeType Type;
 
     private void Awake() {
         YPosition = transform.position.y;
+        XPosition = transform.position.x;
         NodeTag = tag;
         Type = GetComponent<INodeType>().GetNodeType();
     }
