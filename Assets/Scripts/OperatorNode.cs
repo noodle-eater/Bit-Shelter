982f42cfaef349f3d62b187ee62fb2034ba0c607
AmdHamdani <Hamdaniryuuken@gmail.com>
2020-06-17 22:54:18 +0700

    Implement IInitOnStart

--- Assets/Scripts/OperatorNode.cs
+++ Assets/Scripts/OperatorNode.cs
@@ -1,40 +1,45 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class OperatorNode : MonoBehaviour, INodeType
+public class OperatorNode : MonoBehaviour, INodeType, IInitOnStart
 {
 
     public OperatorType comparator;
-    private InputNode[] inputs;
+    private int[] inputs = new int[2];
 
     public NodeData OperatorData { get; private set; }
 
-    private void Start()
+    public void InitOnStart()
     {
-        inputs = FindObjectsOfType<InputNode>();
         OperatorData = GetComponent<NodeData>();
-    }
-
-    public bool GetResult() {
-        switch(comparator) {
-            case OperatorType.Or : return inputs[0].IsActive || inputs[1].IsActive;
-            case OperatorType.And : return inputs[0].IsActive && inputs[1].IsActive;
-            // case OperatorType.Not : 
-            case OperatorType.NotOr : return !(inputs[0].IsActive || inputs[1].IsActive);
-            case OperatorType.NotAnd : return !(inputs[0].IsActive && inputs[1].IsActive);
-            default : return false;
-        }
-        
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
         var connector = other.GetComponent<Connector>();
         connector.AddOperator(this);
     }
+    
+    public bool GetResult() {
+        switch(comparator) {
+            case OperatorType.Or : return ToBool(inputs[0]) || ToBool(inputs[1]);
+            case OperatorType.And : return ToBool(inputs[0]) && ToBool(inputs[1]);
+            // case OperatorType.Not : 
+            case OperatorType.NotOr : return !(ToBool(inputs[0]) || ToBool(inputs[1]));
+            case OperatorType.NotAnd : return !(ToBool(inputs[0]) && ToBool(inputs[1]));
+            default : return false;
+        }
+        
+    }
+
+    public int GetIntResult() {
+        return 0;
+    }
 
     public NodeType GetNodeType()
     {
         return NodeType.Operator;
+    }
+
+    private bool ToBool(int value) {
+        return value == 1;
     }
 }
