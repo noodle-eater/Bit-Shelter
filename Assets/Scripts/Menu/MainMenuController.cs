60d4e9cb5ded8317ae2b15a8331807d24f712f28
AmdHamdani <Hamdaniryuuken@gmail.com>
2020-06-22 22:19:02 +0700

    Add Main Menu Controller

---
+++ Assets/Scripts/Menu/MainMenuController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour
+{
+
+    public Button playButton;
+    public Button settingButton;
+    public Button aboutButton;
+    public Text levelSelect;
+    public Button reduceButton;
+    public Button addButton;
+
+    private int levelNumber = 1;
+
+    private void Start()
+    {
+        playButton.onClick.AddListener(() => SceneManager.LoadScene("Level " + levelNumber));
+        settingButton.onClick.AddListener(() => Debug.Log("Open Setting"));
+        aboutButton.onClick.AddListener(() => Debug.Log("About Button"));
+        addButton.onClick.AddListener(() => { levelNumber++; levelSelect.text = levelNumber.ToString(); });
+        reduceButton.onClick.AddListener(() => { 
+            if(levelNumber > 1) {
+                levelNumber--; 
+                levelSelect.text = levelNumber.ToString(); 
+            }
+        });
+    }
+
+}
