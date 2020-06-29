d42e50776054676c16caedd3684fa98501115b46
AmdHamdani <Hamdaniryuuken@gmail.com>
2020-06-29 22:23:12 +0700

    Add menu listener

--- Assets/Scripts/Menu/MainMenuController.cs
+++ Assets/Scripts/Menu/MainMenuController.cs
@@ -11,14 +11,19 @@ public class MainMenuController : MonoBehaviour
     public Text levelSelect;
     public Button reduceButton;
     public Button addButton;
+    public Button backAboutButton;
 
     private int levelNumber = 1;
+    private BackButtonController back;
 
     private void Start()
     {
+        back = GetComponent<BackButtonController>();
+
         playButton.onClick.AddListener(() => SceneManager.LoadScene("Level " + levelNumber));
-        settingButton.onClick.AddListener(() => Debug.Log("Open Setting"));
-        aboutButton.onClick.AddListener(() => Debug.Log("About Button"));
+        settingButton.onClick.AddListener(() => back.ChangeMenu(MenuType.Setting));
+        aboutButton.onClick.AddListener(() => back.ChangeMenu(MenuType.About));
+        backAboutButton.onClick.AddListener(() => back.ChangeMenu(MenuType.Main));
         addButton.onClick.AddListener(() => { levelNumber++; levelSelect.text = levelNumber.ToString(); });
         reduceButton.onClick.AddListener(() => { 
             if(levelNumber > 1) {
