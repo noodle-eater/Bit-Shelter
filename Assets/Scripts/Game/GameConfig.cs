15cfd4a012998dafc2bcf827e47ec47879a79820
AmdHamdani <Hamdaniryuuken@gmail.com>
2020-06-29 21:57:03 +0700

    Add GameConfig

---
+++ Assets/Scripts/Game/GameConfig.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "GameConfig", menuName = "Bit Shelter/GameConfig", order = 0)]
+public class GameConfig : ScriptableObject {
+    
+    public int maxLevel;
+    public int currentLevel;
+    public float bgmVolume;
+    public float sfxVolume;
+
+    public static GameConfig Instance {
+        get {
+            return Resources.Load<GameConfig>("Game Config");
+        }
+    }
+
+}
