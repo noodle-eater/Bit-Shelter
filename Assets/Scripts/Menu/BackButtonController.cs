4bbeb1ee838293a28ee81d2409c37380945f79f0
AmdHamdani <Hamdaniryuuken@gmail.com>
2020-06-29 22:22:58 +0700

    Add back button controller

---
+++ Assets/Scripts/Menu/BackButtonController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BackButtonController : MonoBehaviour
+{
+
+    public GameObject mainMenu;
+    public GameObject settingMenu;
+    public GameObject aboutMenu;
+    public MenuType menuType;
+
+    public void ChangeMenu(MenuType type) {
+        menuType = type;
+
+        switch(type) {
+            case MenuType.About: 
+                aboutMenu.SetActive(true);
+                settingMenu.SetActive(false);
+                mainMenu.SetActive(false);
+                break;
+            case MenuType.Setting:
+                settingMenu.SetActive(true);
+                aboutMenu.SetActive(false);
+                mainMenu.SetActive(false);
+                break;
+            case MenuType.Main:
+                mainMenu.SetActive(true);
+                settingMenu.SetActive(false);
+                aboutMenu.SetActive(false);
+                break;
+        }
+    }
+
+}
