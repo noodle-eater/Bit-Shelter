16c7fe7697aab52da615dd738b0c4c14e6f3124c
AmdHamdani <Hamdaniryuuken@gmail.com>
2020-07-01 20:22:25 +0700

    Animate Menu

--- Assets/Scripts/Menu/BackButtonController.cs
+++ Assets/Scripts/Menu/BackButtonController.cs
@@ -1,36 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class BackButtonController : MonoBehaviour
 {
 
-    public GameObject mainMenu;
-    public GameObject settingMenu;
-    public GameObject aboutMenu;
-    public MenuType menuType;
+    public RectTransform mainMenu;
+    public RectTransform settingMenu;
+    public RectTransform aboutMenu;
+    public RectTransform loadingMenu;
 
-    public void ChangeMenu(MenuType type) {
-        menuType = type;
+    private MenuType currentType;
+    private TweenConfigMenu tweenConfig;
 
-        switch(type) {
-            case MenuType.About: 
-                aboutMenu.SetActive(true);
-                settingMenu.SetActive(false);
-                mainMenu.SetActive(false);
+    private void Awake()
+    {
+        tweenConfig = TweenConfigMenu.Instance;
+    }
+    public void ChangeMenu(MenuType type)
+    {
+        switch (type)
+        {
+            case MenuType.About:
+                aboutMenu.DOAnchorPos(Vector2.zero, 1, false);
+                mainMenu.DOAnchorPos(new Vector2(-800, 0), 1, false);
                 break;
             case MenuType.Setting:
-                settingMenu.SetActive(true);
-                aboutMenu.SetActive(false);
-                mainMenu.SetActive(false);
+                settingMenu.DOAnchorPos(Vector2.zero, 1, false);
+                mainMenu.DOAnchorPos(new Vector2(800, 0), 1, false);
                 break;
             case MenuType.Main:
-                mainMenu.SetActive(true);
-                settingMenu.SetActive(false);
-                aboutMenu.SetActive(false);
+                mainMenu.DOAnchorPos(Vector2.zero, 1, false);
+                switch (currentType)
+                {
+                    case MenuType.About:
+                        aboutMenu.DOAnchorPos(new Vector2(800, 0), 1, false);
+                        break;
+                    case MenuType.Setting:
+                        settingMenu.DOAnchorPos(new Vector2(-800, 0), 1, false);
+                        break;
+                }
+                break;
+            case MenuType.Loading:
+                loadingMenu.DOAnchorPos(Vector2.zero, 1, false);
+                mainMenu.DOAnchorPos(new Vector2(0, -450), 1, false);
                 break;
         }
+
+        currentType = type;
     }
 
+    private void ChangeMenu(ref RectTransform rect, MenuConfig config)
+    {
+        rect.DOAnchorPos(config.targetPosition, config.duration, config.snap);
+    }
 }
