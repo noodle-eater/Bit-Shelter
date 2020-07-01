e6740d0eb7771bb88f306123a85f6929c9d16447
AmdHamdani <Hamdaniryuuken@gmail.com>
2020-07-01 08:05:02 +0700

    Add DoTween

---
+++ Assets/Scripts/Core/SingletonScriptable.cs
@@ -0,0 +1,11 @@
+// using UnityEngine;
+
+// public class SingletonScriptable<T> where T : ScriptableObject {
+
+//     public static T Instance {
+//         get {
+//             return Resources.Load
+//         }
+//     }
+
+// }
---
+++ Assets/Scripts/Menu/TweenConfigMenu.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "MenuTweenConfig", menuName = "Bit Shelter/MenuTweenConfig", order = 0)]
+public class TweenConfigMenu : ScriptableObject
+{
+
+    [SerializeField]
+    private MenuConfig mainMenu;
+    [SerializeField]
+    private MenuConfig aboutMenu;
+    [SerializeField]
+    private MenuConfig settingMenu;
+    [SerializeField]
+    private MenuConfig loadingMenu;
+
+    public MenuConfig MainMenu { get => mainMenu; }
+    public MenuConfig AboutMenu { get => aboutMenu; }
+    public MenuConfig SettingMenu { get => settingMenu; }
+    public MenuConfig LoadingMenu { get => loadingMenu; }
+
+    public static TweenConfigMenu Instance {
+        get {
+            return Resources.Load<TweenConfigMenu>("MenuTweenConfig");
+        }
+    }
+}
+
+[System.Serializable]
+public class MenuConfig
+{
+    public Vector2 targetPosition = Vector2.zero;
+    public float duration = 0f;
+    public bool snap = false;
+}
