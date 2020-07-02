61e432f0f487b0058819bc2bed226dabef96d6da
AmdHamdani <Hamdaniryuuken@gmail.com>
2020-07-02 07:41:37 +0700

    Add GameSettingMenu

---
+++ Assets/Scripts/Editor/GameSettingMenu.cs
@@ -0,0 +1,25 @@
+using UnityEditor;
+using UnityEngine;
+
+public class GameSettingMenu {
+    
+    [MenuItem("Game/Set Environment to Sandbox")]
+    public static void SetEnvironmentToSandbox() {
+        PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.WebGL, "SANDBOX");
+        Debug.Log("<color=green>Environment is set to Sandbox</color>");
+    }
+
+    [MenuItem("Game/Set Environment to Production")]
+    public static void SetEnvironmentToProduction() {
+        PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.WebGL, "PRODUCTION");
+        Debug.Log("<color=green>Environment is set to Production</color>");
+    }
+
+    [MenuItem("Game/Reset GameConfig")]
+    public static void ResetGameConfig() {
+        GameConfig.Instance.bgmVolume = 1;
+        GameConfig.Instance.sfxVolume = 1;
+        GameConfig.Instance.currentLevel = 1;
+    }
+
+}
