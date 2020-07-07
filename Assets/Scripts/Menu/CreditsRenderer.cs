73966cf999fcee7f916afcc6055446f05ae6e6ee
AmdHamdani <Hamdaniryuuken@gmail.com>
2020-07-07 23:01:27 +0700

    Add CreditsRenderer

---
+++ Assets/Scripts/Menu/CreditsRenderer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreditsRenderer : MonoBehaviour, IInitOnStart
+{
+
+    [SerializeField]
+    private Text aboutText;
+    [SerializeField]
+    private TextAsset creditsFile;
+
+    public void InitOnStart()
+    {
+        aboutText.text = creditsFile.text;    
+    }
+}
