c0c8e1d01ebbc24882a15aa243dca1c419d1bf83
AmdHamdani <Hamdaniryuuken@gmail.com>
2020-07-08 22:29:30 +0700

    Add SetVolume on volume slider value changed

--- Assets/Scripts/Menu/SettingMenuController.cs
+++ Assets/Scripts/Menu/SettingMenuController.cs
@@ -9,15 +9,26 @@ public class SettingMenuController : MonoBehaviour
     public Slider bgmVolume;
     public Slider sfxVolume;
     public Button backButton;
+    public AudioPlayer audioPlayer;
 
     // Start is called before the first frame update
     private void Start()
     {
+        audioPlayer = FindObjectOfType<AudioPlayer>();
+
         bgmVolume.value = GameConfig.Instance.bgmVolume;
         sfxVolume.value = GameConfig.Instance.sfxVolume;
 
-        bgmVolume.onValueChanged.AddListener((value) => GameConfig.Instance.bgmVolume = value);
-        sfxVolume.onValueChanged.AddListener((value) => GameConfig.Instance.sfxVolume = value);
+        bgmVolume.onValueChanged.AddListener((value) =>
+        {
+            GameConfig.Instance.bgmVolume = value;
+            audioPlayer.SetVolume();
+        });
+        sfxVolume.onValueChanged.AddListener((value) =>
+        {
+            GameConfig.Instance.sfxVolume = value;
+            audioPlayer.SetVolume();
+        });
 
         backButton.onClick.AddListener(() => GetComponent<BackButtonController>().ChangeMenu(MenuType.Main));
     }
@@ -25,6 +36,6 @@ public class SettingMenuController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
