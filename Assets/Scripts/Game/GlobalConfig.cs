0ac2431043d0e216f737aebd8e8147119add0ef5
AmdHamdani <Hamdaniryuuken@gmail.com>
2020-07-10 22:52:13 +0700

    Add GlobalConfig

---
+++ Assets/Scripts/Game/GlobalConfig.cs
@@ -0,0 +1,6 @@
+public class GlobalConfig {
+    public static int MaxLevel = 20;
+    public static int CurrentLevel = 1;
+    public static float BgmVolume = .5f;
+    public static float SfxVolume = .5f;
+}
